57ceebf2b4c89d5ea22607ed4fe1660cd2501c93
Marko Mitrovic <mitrovim@gmail.com>
2025-11-29 21:06:58 -0700

    Startup fixes

--- src/context/storage-context/index.tsx
+++ src/context/storage-context/index.tsx
@@ -7,6 +7,7 @@ const useBackend = import.meta.env.VITE_USE_BACKEND === 'true';
 export const StorageProvider: React.FC<React.PropsWithChildren> = ({
     children,
 }) => {
+    console.log('useBackend:', useBackend);
     if (useBackend) {
         return <ApiStorageProvider>{children}</ApiStorageProvider>;
     }
@@ -14,8 +15,8 @@ export const StorageProvider: React.FC<React.PropsWithChildren> = ({
     return <DexieStorageProvider>{children}</DexieStorageProvider>;
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
-export { useStorage } from './use-storage';
+// // eslint-disable-next-line react-refresh/only-export-components
+// export { useStorage } from './use-storage';
 // eslint-disable-next-line react-refresh/only-export-components
 export { storageContext } from './storage-context';
 export type { StorageContext } from './storage-context';
